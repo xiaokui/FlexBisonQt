1896e799db790639ba7ad5308ee53128b964d9f3
xiaokui <xiaokui.zhao@163.com>
2017-05-03 08:47:09 +0800

    完善 例子3的

--- Example1/main.cpp
+++ Example1/main.cpp
@@ -17,28 +17,7 @@ int main(int argc, char *argv[])
 
     yylex();
 
-
-
     fclose(yyin);
-
-    /*
-    QString str("{'one': 1,"
-                "\"two\": '2',"
-                "\"'3'\": [3.14, 10, [7, 8], \"\\\"quoted string\\\"\"],"
-                "'4': size(640, 480),"
-                "'date': dateTime(date(2013, 2, 27), time(12, 0)),"
-                "'number count': {'1': 1, '2': 2, '3': 3}}");
-
-    // Insert the string into the input stream.
-    YY_BUFFER_STATE bufferState = yy_scan_string(str.toUtf8().constData());
-
-    // Parse the string.
-    yyparse();
-
-    // flush the input stream.
-    yy_delete_buffer(bufferState);
-
-    */
 
     return app.exec();
 }
